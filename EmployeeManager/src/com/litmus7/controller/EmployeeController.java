d8048804b9cefb33bca3556ba10ae56a8b452e30
Augustine Joel Joseph <115809487+Joppan2002@users.noreply.github.com>
2025-08-13 11:44:33 +0530

    Update EmployeeController.java

--- EmployeeManager/src/com/litmus7/controller/EmployeeController.java
+++ EmployeeManager/src/com/litmus7/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.litmus7.employeemanager.util.TextFileUtil;
 import com.litmus7.employeemanager.util.ValidationUtil;
 import com.litmus7.employeemanager.util.Response;
 import com.litmus7.employeemanager.services.EmployeeService;
+import com.litmus7.exceptions.EmployeeNotFoundException;
+import com.litmus7.exceptions.EmployeeServiceException;
 
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -72,60 +74,83 @@ public class EmployeeController {
     }
     
     
-    //second part
-    
-    
-    EmployeeService service=new EmployeeService();
-    
-    
-    public Boolean ValidationCheck(Employee emp)
-    {
-    	if(ValidationUtil.validateEmployee(emp).equalsIgnoreCase("valid"))
-    	{
-    		return true;
-    	}
-    	else
-    	{
-    		return false;
-    	}
+ // second part
+
+    EmployeeService service = new EmployeeService();
+
+    public Boolean ValidationCheck(Employee emp) throws EmployeeServiceException {
+        try {
+            return ValidationUtil.validateEmployee(emp).equalsIgnoreCase("valid");
+        } catch (Exception e) {
+            throw new EmployeeServiceException("Error validating employee data", e);
+        }
     }
-    
-    
-    public boolean inserttoEmployee(Employee emp)
-    {
-    	return service.addEmployee(emp).getApplicationStatus();
+
+    public boolean inserttoEmployee(Employee emp) throws EmployeeServiceException {
+        if (emp == null) {
+            throw new EmployeeServiceException("Cannot insert null Employee");
+        }
+        try {
+            service.addEmployee(emp);
+            return true;
+        } catch (Exception e) {
+            throw new EmployeeServiceException("Failed to insert employee", e);
+        }
     }
-    
-    
-    
-    public List<Employee> fetchAllEmployees()
-    {
-    	List<Employee> employees = new ArrayList<>();
-    	employees=service.RetrieveAll().getData();
-    	
-    	if(employees.size()==0)
-    	{
-    		return null;
-    	}
-    	return employees;
+
+    public List<Employee> fetchAllEmployees() throws EmployeeServiceException, EmployeeNotFoundException {
+        try {
+            List<Employee> employees = service.RetrieveAll().getData();
+            if (employees == null || employees.isEmpty()) {
+                throw new EmployeeNotFoundException("No employees found");
+            }
+            return employees;
+        } catch (EmployeeNotFoundException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new EmployeeServiceException("Error fetching all employees", e);
+        }
     }
-    
-    
-    public Employee fetchWithId(int id)
-    {
-    	return service.RetrieveOne(id).getData();
+
+    public Employee fetchWithId(int id) throws EmployeeServiceException, EmployeeNotFoundException {
+        try {
+            Employee emp = service.RetrieveOne(id).getData();
+            if (emp == null) {
+                throw new EmployeeNotFoundException("Employee with ID " + id + " not found");
+            }
+            return emp;
+        } catch (EmployeeNotFoundException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new EmployeeServiceException("Error fetching employee with ID: " + id, e);
+        }
     }
-    
-    
-    public boolean deletionInEmployee(int id)
-    {
-    	return service.DeleteByID(id).getApplicationStatus();
+
+    public boolean deletionInEmployee(int id) throws EmployeeServiceException, EmployeeNotFoundException {
+        try {
+            boolean status = service.DeleteByID(id).getApplicationStatus();
+            if (!status) {
+                throw new EmployeeNotFoundException("Employee with ID " + id + " not found for deletion");
+            }
+            return true;
+        } catch (EmployeeNotFoundException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new EmployeeServiceException("Error deleting employee with ID: " + id, e);
+        }
     }
-    
-    
-    public boolean nameUpdation(int ID,String newFirstName,String newLastName)
-    {
-    	return service.UpdateName(ID, newFirstName, newLastName).getApplicationStatus();
+
+    public boolean nameUpdation(int ID, String newFirstName, String newLastName) throws EmployeeServiceException, EmployeeNotFoundException {
+        try {
+            boolean status = service.UpdateName(ID, newFirstName, newLastName).getApplicationStatus();
+            if (!status) {
+                throw new EmployeeNotFoundException("Employee with ID " + ID + " not found for name update");
+            }
+            return true;
+        } catch (EmployeeNotFoundException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new EmployeeServiceException("Error updating employee name for ID: " + ID, e);
+        }
     }
-    
 }
