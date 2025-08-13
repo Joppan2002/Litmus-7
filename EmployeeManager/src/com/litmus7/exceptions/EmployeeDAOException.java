40f8dbef10f0fbcc1ffd7c99866d54f6c56939c6
Augustine Joel Joseph <115809487+Joppan2002@users.noreply.github.com>
2025-08-13 11:48:21 +0530

    Create EmployeeDAOException.java

---
+++ EmployeeManager/src/com/litmus7/exceptions/EmployeeDAOException.java
@@ -0,0 +1,11 @@
+package com.litmus7.exceptions;
+
+
+
+public class EmployeeDAOException extends Exception 
+{
+	public EmployeeDAOException(String message,Throwable cause)
+	{
+		super(message,cause);
+	}
+}
