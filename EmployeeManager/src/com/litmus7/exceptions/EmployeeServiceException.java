81452c609319db5e4ee7852d87ccf6b7f058171d
Augustine Joel Joseph <115809487+Joppan2002@users.noreply.github.com>
2025-08-17 23:11:39 +0530

    Update EmployeeServiceException.java

--- EmployeeManager/src/com/litmus7/exceptions/EmployeeServiceException.java
+++ EmployeeManager/src/com/litmus7/exceptions/EmployeeServiceException.java
@@ -1,12 +1,20 @@
-package com.litmus7.exceptions;
+package com.litmus7.employeemanager.exceptions;
 
-public class EmployeeServiceException extends Exception {
+public class EmployeeServiceException extends Exception 
+{
+	String errorCode;
+	
+	public EmployeeServiceException (String errorCode, String message, Throwable cause) {
+		super(message, cause);
+		this.errorCode=errorCode;
+	}
 	
 	public EmployeeServiceException (String message) {
 		super(message);
 	}
 	
-	public EmployeeServiceException (String message, Throwable cause) {
-		super(message, cause);
+	String getErrorCode()
+	{
+		return errorCode;
 	}
 }
