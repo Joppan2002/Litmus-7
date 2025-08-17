0618e75a464db2741717eb849d94ce5ddafac2d4
Augustine Joel Joseph <115809487+Joppan2002@users.noreply.github.com>
2025-08-17 23:11:19 +0530

    Update EmployeeNotFoundException.java

--- EmployeeManager/src/com/litmus7/exceptions/EmployeeNotFoundException.java
+++ EmployeeManager/src/com/litmus7/exceptions/EmployeeNotFoundException.java
@@ -1,9 +1,17 @@
-package com.litmus7.exceptions;
+package com.litmus7.employeemanager.exceptions;
 
 public class EmployeeNotFoundException extends Exception 
 {
-	public EmployeeNotFoundException(String message) 
+	String errorCode;
+	public EmployeeNotFoundException(String message,String errorCode) 
 	{
 		super(message);
+		this.errorCode=errorCode;
 	}
+	
+	String getErrorCode()
+	{
+		return errorCode;
+	}
+	
 }
