cc64ba7e679c1c5679aa49f7609f726104dab392
Augustine Joel Joseph <115809487+Joppan2002@users.noreply.github.com>
2025-08-17 23:10:57 +0530

    Update EmployeeDAOException.java

--- EmployeeManager/src/com/litmus7/exceptions/EmployeeDAOException.java
+++ EmployeeManager/src/com/litmus7/exceptions/EmployeeDAOException.java
@@ -1,11 +1,26 @@
-package com.litmus7.exceptions;
+package com.litmus7.employeemanager.exceptions;
 
 
 
 public class EmployeeDAOException extends Exception 
 {
+	
+	String errorCode;
+	
 	public EmployeeDAOException(String message,Throwable cause)
 	{
 		super(message,cause);
+	}
+	
+	public EmployeeDAOException(String message,String errorCode, Throwable cause)
+	{
+		super(message,cause);
+		this.errorCode=errorCode;
+		
+	}
+	
+	String getErrorCode()
+	{
+		return errorCode;
 	}
 }
